2e52264933b802993a8e9438b9c28ca5b2424218
3B232093 <3b232093@gm.student.ncut.edu.tw>
2025-12-31 00:40:00 +0800

    cc.c2

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/cc/c1', function () {
     return view('cc.c1');
 });
 
+Route::get('/cc/c2', function () {
+    return view('cc.c2');
+});
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
